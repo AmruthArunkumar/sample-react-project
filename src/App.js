7deb70d4d6f466f53ef249210df5ae8d841ae06e
Amruth Arunkumar <amruth1.618@gmail.com>
2025-02-27 14:35:06 -0500

    readme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' />
-        </Routes>
-      </Router>
-    </div>
-  )
+    return (
+        <div>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import {React, StrictMode} from 'react'
+import { React, StrictMode } from "react";
 
-import App from './App'
+import App from "./App";
 
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
 // This is the ID of the div in your index.html file
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
     <StrictMode>
-      <App />
-    </StrictMode>,
-   
+        <App />
+    </StrictMode>
 );
-
