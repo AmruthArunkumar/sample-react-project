84518c1b2443b62f1393e27ee53cc12a3e117c9a
Amruth Arunkumar <58234150+AmruthArunkumar@users.noreply.github.com>
2025-02-27 11:34:13 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import {React, StrictMode} from 'react'
+
+import App from './App'
+
+import {createRoot} from 'react-dom/client';
+
+// This is the ID of the div in your index.html file
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+   
+);
+
